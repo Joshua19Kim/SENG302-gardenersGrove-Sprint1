2885871a810bb9f63649e0b114c078c2f44297df
Kush Desai <kde54@uclive.ac.nz>
2024-03-07 21:55:15 +1300

    Moved SecurityConfiguration into configuration folder. Also rewrote some code based on the example repo provided

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/component/SecurityConfiguration.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/component/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package nz.ac.canterbury.seng302.gardenersgrove.configuration;
+package nz.ac.canterbury.seng302.gardenersgrove.component;
 
 import nz.ac.canterbury.seng302.gardenersgrove.component.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 /**
  * Custom Security Configuration
  * Such functionality was previously handled by WebSecurityConfigurerAdapter
@@ -51,38 +51,29 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // Allow h console through security. Note: Spring  broke the nicer way to do this (i.e. how the authorisation is
-        // handled below)
-        // See https://github.com/spring-projects/spring-security/issues/
-        http.authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h/**")).permitAll())
-                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")));
+        // Allow h2 console through security. Note: Spring 6 broke the nicer way to do this (i.e. how the authorisation is handled below)
+        // See https://github.com/spring-projects/spring-security/issues/12546
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")).permitAll())
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable())
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")))
 
-        // Allow "/", "/register", and "/login" to anyone (permitAll)
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/register", "/login").permitAll()
-                // Only allow admins to reach the "/admin" page
-                .requestMatchers("/admin")
-                // note we do not need the "ROLE_" prefix as we are calling "hasRole()"
-                .hasRole("ADMIN")
-                // Any other request requires authentication
-                .anyRequest().authenticated());
-
-        // Define logging in, a POST "/login" endpoint now exists under the hood, after login redirect to user page
-        http.formLogin(formLogin -> formLogin
-                .loginPage("/login").permitAll()
-                .loginProcessingUrl("/login")
-                .failureUrl("/login?FAILED")
-                .defaultSuccessUrl("/userProfile"));
-
-        // Define logging out, a POST "/logout" endpoint now exists under the hood, redirect to "/login", invalidate session and remove cookie
-        http.logout(lOut -> {
-                    lOut.invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll()
-                    .deleteCookies("JSESSIONID");
-        });
+                .authorizeHttpRequests(request ->
+                        // Allow "/", "/register", and "/login" to anyone (permitAll)
+                        request.requestMatchers("/", "/register", "/login")
+                                .permitAll()
+                                // Only allow admins to reach the "/admin" page
+                                .requestMatchers("/admin")
+                                .hasRole("ADMIN")
+                                // Any other request requires authentication
+                                .anyRequest()
+                                .authenticated()
+                )
+                // Define logging in, a POST "/login" endpoint now exists under the hood, after login redirect to user page
+                .formLogin(formLogin -> formLogin.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/user"))
+                // Define logging out, a POST "/logout" endpoint now exists under the hood, redirect to "/login", invalidate session and remove cookie
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login").invalidateHttpSession(true).deleteCookies("JSESSIONID"));
         return http.build();
+
 
     }
 }
