040f9f8c79fcdbed93ce8d40b9dabd2e64f838bb
Kush Desai <kde54@uclive.ac.nz>
2024-03-13 17:48:07 +1300

    updated default users as per the README

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('Kush', 'Desai', DATE '2004-01-07', 'kush@gmail.com', 49, 'defaultProfilePic.png');
-INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('Ben', 'Desai', DATE '2004-01-07', 'ben@gmail.com', 2, 'defaultProfilePic.png');
-INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('Sam', 'Desai', DATE '2004-01-07', 'sam@gmail.com', 3, 'defaultProfilePic.png');
-INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('Josh', 'Desai', DATE '2004-01-07', 'josh@gmail.com', 4, 'defaultProfilePic.png');
+INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('A', 'A', DATE '2004-01-07', 'a@gmail.com', -1009294837, 'defaultProfilePic.png');
+INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('B', 'B', DATE '2004-01-07', 'b@gmail.com', -1009294837, 'defaultProfilePic.png');
+INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('C', 'C', null, 'c@gmail.com', -1009294837, 'defaultProfilePic.png');
+INSERT INTO gardener (first_name, last_name, DoB, email, password, profile_picture) VALUES ('D', null, DATE '2004-01-07', 'd@gmail.com', -1009294837, 'defaultProfilePic.png');
